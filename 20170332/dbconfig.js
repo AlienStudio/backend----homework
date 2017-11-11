9bba1a6e0e76e0ab7a785e778b0c616046e9f389
xyl <ggrd007@gmail.com>
2017-11-11 18:01:08 +0800

    Update

--- 20170332/dbconfig.js
+++ 20170332/dbconfig.js
@@ -2,7 +2,7 @@
 var config = {
     database: 'vote',
     username: 'root',
-    password: 'toor',
+    password: 'lzhxyl47',
     host: '127.0.0.1',
     port: 3306
 };
--- 20170332/server.js
+++ 20170332/server.js
@@ -1,42 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var server = express();
-var dbEditor = require("./dbEditor");
-
 server.listen(80);
 server.use(bodyParser.urlencoded({extended: false}));
-var path = "/vote"
-server.get(path,function (req,res) {
-    var obj = dbEditor.doGet(rebuild(req.query));
+console.log("Started");
 
-    res.send(obj);
-    res.end();
-});
-server.put(path,function (req,res) {
-    var data = dbEditor.doPut(req.body);
-    res.send(data);
-    res.end();
-});
-server.post(path,function (req,res) {
-    var obj = dbEditor.doPost(rebuild(req.body));
-    res.send(obj);
-    res.end();
-});
-server.use("/",function (req,res) {
-    res.sendStatus(200);
-    res.end();
-});
-function rebuild (obj1){
-    var obj={
-        name:"",
-        boolarr:new Array()
-    }
-    obj.name=obj1.name;
-    obj.boolarr[0]=obj1.Vote0;
-    obj.boolarr[1]=obj1.Vote1;
-    obj.boolarr[2]=obj1.Vote2;
-    obj.boolarr[3]=obj1.Vote3;
-    obj.boolarr[4]=obj1.Vote4;
-    obj.boolarr[5]=obj1.Vote5;
-}
+server.get('/vote', function (req, res) {
+    console.log("/vote:get");
+    res.end('Get');
+})
 
+server.put('/vote', function (req, res) {
+    console.log("/vote:put");
+    res.end('Put');
+    JSON.parse()
+})
+
+server.post('/vote', function (req, res) {
+    console.log("/vote:post");
+    res.end('Post');
+})
+
+server.get('/vote/status', function (req, res) {
+    console.log("/vote/status:get");
+    res.end('Get')
+})
